0099a8cca674fa08c71d370a8a4e4332055047dd
Peggy Kalliani <pegztales@gmail.com>
2023-03-09 16:38:35 +0200

    adding scenes/pages and dark mode theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 
 function App() {
+  const [theme, colorMode] = useMode();
+  
   return (
-    <div className="app">
-      <h1>Hello World</h1>
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme ={theme}> 
+        <CssBaseline />
+
+        <div className="app">
+         <main className = "content"> </main>     
+        </div>
+        
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
